fc58eb470c6f330cc17f580a9429008e3e6d1636
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2021-07-22 17:40:44 +0300

    Adding new patch - owner referrence for kubernetes logs

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cargo install cross
 git clone --depth 1 --branch v0.15.0 https://github.com/timberio/vector.git
 cd $VECTOR_PATH
 git apply ${CUR_PATH}/loki-labels.patch
+git apply ${CUR_PATH}/kubernetes_logs-owner-ref.patch
 # specify packages you need
 for package in `echo "package-x86_64-unknown-linux-musl"`; do
     FEATURES=default make $package
