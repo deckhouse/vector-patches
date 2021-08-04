594e18ddece8e705051155b417470a36e3c8eccd
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2021-08-04 13:53:58 +0300

    Adding fresh patches

--- build.sh
+++ build.sh
@@ -2,21 +2,27 @@
 
 CUR_PATH=$(pwd)
 VECTOR_PATH=${CUR_PATH}/vector
+DOCKERREPO=${REPO:-flant/vector}
 if [ ! -e $HOME/.cargo/env ]; then
   echo "Installing rust toolchain"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
 fi
 source "$HOME/.cargo/env"
 cargo install cross
-git clone --depth 1 --branch v0.15.0 https://github.com/timberio/vector.git
-cd $VECTOR_PATH
+if [ ! -e $VECTOR_PATH ]; then
+  git clone --depth 1 --branch v0.15.1 https://github.com/timberio/vector.git
+  cd $VECTOR_PATH
+else
+  cd $VECTOR_PATH
+  git checkout .
+fi
 git apply ${CUR_PATH}/loki-labels.patch
-git apply ${CUR_PATH}/kubernetes_logs-lib.patch
+#git apply ${CUR_PATH}/kubernetes_logs-lib.patch
 git apply ${CUR_PATH}/kubernetes_logs-owner-ref.patch
 # specify packages you need
 for package in `echo "package-x86_64-unknown-linux-musl"`; do
     FEATURES=default make $package
 done
-FEATURES=default REPO=flant/vector make release-docker
+FEATURES=default REPO=${DOCKERREPO} make release-docker
 cd ${CUR_PATH}
-rm -rf $VECTOR_PATH 
+#rm -rf $VECTOR_PATH
